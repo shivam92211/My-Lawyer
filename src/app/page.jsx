a7b799ba8e7dc917fd046ed72d6486d93b197b13
Shivam Vishwakarma <shivam92211@gmail.com>
2024-04-03 13:34:18 +0530

    balance image added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,23 +11,29 @@ const Homepage = () => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 md:justify-between">
         {/* IMAGE CONTAINER */}
-        <div className="h-1/2 lg:h-full lg:w-1/2 relative">
-          <Image src="/hero.png" alt="" fill className="object-contain" />
+        <div className="h-1/2 lg:h-full lg:w-1/2 relative lg:mr-8 mb-24">
+          <Image src="/balance.png" alt="" fill className="object-contain" />
         </div>
         {/* TEXT CONTAINER */}
         <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
           {/* TITLE */}
           <h1 className="text-4xl md:text-6xl font-bold">
-            Crafting Digital Experiences, Designing Tomorrow.
+            Welcome to My Lawyer.
           </h1>
           {/* DESC */}
           <p className="md:text-xl">
-            Welcome to my digital canvas, where innovation and creativity
-            converge. With a keen eye for aesthetics and a mastery of code, my
-            portfolio showcases a diverse collection of projects that reflect my
-            commitment to excellence.
+            where we understand that navigating legal matters can be daunting, 
+            especially in the aftermath of a crime or illegal activity. 
+            Our mission is simple: to provide you with expert guidance and 
+            support when you need it most. Whether you're facing complex legal
+            issues or simply seeking advice on the next steps to take,
+            our team of experienced lawyers is here to help you navigate
+            through the legal process with confidence and clarity. 
+            Trust in us to be your steadfast advocates, guiding you towards
+            the best possible outcome. Let us shoulder the legal burden so 
+            you can focus on moving forward. Your peace of mind starts here.
           </p>
           {/* BUTTONS */}
           <div className="w-full flex gap-4">
