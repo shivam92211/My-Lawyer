a6696085c1f89095f95766fa7825aa260897701e
Shivam Vishwakarma <shivam92211@gmail.com>
2024-04-04 10:49:53 +0530

    full name changet to text

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -68,10 +68,11 @@ const ContactPage = () => {
           className="h-1/2 lg:h-full lg:w-1/2 bg-red-50 rounded-xl text-xl flex flex-col gap-8 justify-center p-24"
         >
           <span>Enter Your Full Name</span>
-          <textarea
-            rows={6}
-            className="bg-transparent border-b-2 border-b-black outline-none resize-none"
+          <input
             name="user_message"
+            type="text"
+            className="bg-transparent border-b-2 border-b-black outline-none resize-none"
+            
           />
           <span>Enter Email address</span>
           <input
